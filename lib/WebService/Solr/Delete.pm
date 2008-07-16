b43429c19ff49b91bceccac7fffecd2236984805
Kirk Beers <jed_beers@hotmail.com>
2008-07-16 18:10:58 +0000

    Added a test file for WebService::Solr::Field.pm

--- lib/WebService/Solr/Delete.pm
+++ lib/WebService/Solr/Delete.pm
@@ -1,13 +1,11 @@
 package WebService::Solr::Delete;
 use XML::Generator;
-# Inheriting from Update package
-#use base qw(Update);
 use strict;
 use warnings;
 
 sub new {
     my ( $class, %options ) = @_;
-    if ($options{'id'} && $options{'query'}){
+    if (\%options{'id'} && \options{'query'}){
         die "Must delete by id OR query, not both";
     }
     my $self = {
@@ -17,10 +15,11 @@ sub new {
         bless $self, $class;
         return $self;
 }
-
+1;
 sub delete_by_id{
     my $self = shift;
     my $opts = $self->{opts};
+     die "Expected hash reference for WebService::Solr::Delete->delete_by_id " unless ref($opts) eq "HASH";
     my %optshash = %$opts;
     my $gen = XML::Generator->new(':pretty');
     my $id ='';
@@ -52,5 +51,11 @@ sub delete_by_query{
     );  
     print "\n"; 
 }
-
+package main;
+use strict;
+use warnings;
+use XML::Generator;
+my %opt_3 = (id=>"0001");
+my $delete_3 = WebService::Solr::Delete->new(%opt_3);
+my $got_3 = WebService::Solr::Delete->delete_by_id;
 

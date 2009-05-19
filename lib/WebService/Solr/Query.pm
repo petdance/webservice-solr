dad9a267d3b54763f97243cb843c84c5b0a82ccc
Brian Cassidy <bricas@cpan.org>
2009-05-19 11:50:15 -0300

    start of abstract query interface

---
+++ lib/WebService/Solr/Query.pm
@@ -0,0 +1,36 @@
+package WebService::Solr::Query;
+
+use Moose;
+
+no Moose;
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=head1 NAME
+
+WebService::Solr::Query - Abstract query syntax for Solr queries
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<WebService::Solr>
+
+=back
+
+=head1 AUTHORS
+
+Brian Cassidy E<lt>bricas@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008-2009 National Adult Literacy Database
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself. 
+
+=cut
---
+++ t/query.t
@@ -0,0 +1,9 @@
+use Test::More 'no_plan';
+
+use strict;
+use warnings;
+
+BEGIN {
+    use_ok( 'WebService::Solr::Query' );
+}
+

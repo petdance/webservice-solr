3440ddf0384f7e840b68f07189aa5a5b9936f4cf
Brian Cassidy <bricas@cpan.org>
2009-10-14 14:32:51 -0300

    Ensure compatibility with older versions of Encode by making sure we pass a string to encode()

--- lib/WebService/Solr.pm
+++ lib/WebService/Solr.pm
@@ -150,7 +150,7 @@ sub _send_update {
     my $req = HTTP::Request->new(
         POST => $url,
         HTTP::Headers->new( Content_Type => 'text/xml; charset=utf-8' ),
-        '<?xml version="1.0" encoding="UTF-8"?>' . encode('utf8', $xml)
+        '<?xml version="1.0" encoding="UTF-8"?>' . encode('utf8', "$xml")
     );
 
     my $http_response = $self->agent->request($req);

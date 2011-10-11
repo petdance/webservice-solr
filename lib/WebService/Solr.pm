13cb7de2e9f4052b3fa7b85a63998202166bfbdd
Brian Cassidy <bricas@cpan.org>
2011-10-11 09:07:13 -0300

    prep release

--- lib/WebService/Solr.pm
+++ lib/WebService/Solr.pm
@@ -35,7 +35,7 @@ has 'last_response' => (
     isa => 'Maybe[WebService::Solr::Response]',
 );
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 sub BUILDARGS {
     my ( $self, $url, $options ) = @_;
